a02b8ee5bb47b6d219e10e1bb17db7551421fc6b
ErikPearson <erik_pearson@crgl-thirdparty.com>
2019-02-03 21:49:32 -0600

    updated ddcctl

--- ddcctl.rb
+++ ddcctl.rb
@@ -1,12 +1,18 @@
 # Documentation: http://docs.brew.sh/Formula-Cookbook.html
 #                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
 
+# steps:
+# - at url for zip of latest commit, 
+# - run brew install, 
+# - note the actual SHA from the error message and add it
+# - rerun brew install
+
 class Ddcctl < Formula
   desc "DDC monitor controls (brightness) for Mac OSX command line"
   homepage "https://github.com/kfix/ddcctl"
-  url "https://github.com/kfix/ddcctl/archive/d11c3f4a63a090923b55ec93f363b381f2729d0c.zip"
-  version "20170207"
-  sha256 "ee4b8dedcd6265ea7846688836dfdf31386e953fb8d975d6d7a04546d71e2a7b"
+  url "https://github.com/kfix/ddcctl/archive/4818757471369543e2734ea0080fbacd37a3794b.zip"
+  version "20181207"
+  sha256 "a7bf83d6ba922dba499a9e6aab508b4c279155f1f0a078c6a37ac8248c9adac6"
   head "https://github.com/kfix/ddcctl.git"
 
   def install
